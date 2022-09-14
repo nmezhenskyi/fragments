47008f07a40dafbf254be58f3ed9df2560afd174
Nikita Mezhenskyi <nikita.mezhenskyi@gmail.com>
2022-09-14 10:02:20 -0400

    feat: move route definitions to /routes

--- src/app.js
+++ src/app.js
@@ -3,8 +3,6 @@ const cors = require('cors')
 const helmet = require('helmet')
 const compression = require('compression')
 
-const { version, author } = require('../package.json')
-
 const logger = require('./logger')
 const pino = require('pino-http')({ logger })
 
@@ -14,19 +12,7 @@ app.use(pino)
 app.use(helmet())
 app.use(cors())
 app.use(compression())
-
-/**
- * Health check route. If the server is running, responds with 200 OK.
- */
-app.get('/', (req, res) => {
-  res.setHeader('Cache-Control', 'no-cache')
-  res.status(200).json({
-    status: 'ok',
-    author,
-    githubUrl: 'https://github.com/nmezhenskyi/fragments',
-    version,
-  })
-})
+app.use('/', require('./routes'))
 
 /**
  * Not found handler.
@@ -41,6 +27,9 @@ app.use((req, res) => {
   })
 })
 
+/**
+ * Error handler
+ */
 app.use((err, _req, res, _next) => {
   const status = err.status || 500
   const message = err.message || 'unable to process request'
