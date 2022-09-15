9c69d4b5d999e49df0e403ce1178643bf5ac9236
Nikita Mezhenskyi <nikita.mezhenskyi@gmail.com>
2022-09-14 21:07:35 -0400

    feat: require authentication on routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,12 @@ const express = require('express')
 const { version, author } = require('../../package.json')
 
 const router = express.Router()
+const { authenticate } = require('../authorization')
 
 /**
  * Expose v1 API routes.
  */
-router.use('/v1', require('./api'))
+router.use('/v1', authenticate(), require('./api'))
 
 /**
  * Health check route. If the server is running, responds with 200 OK.
