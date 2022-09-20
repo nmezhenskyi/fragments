b39ad703adb350f184c253a9ebed846db878371b
Nikita Mezhenskyi <nikita.mezhenskyi@gmail.com>
2022-09-19 21:12:37 -0400

    build: add breaking change

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ require('dotenv').config()
 
 const logger = require('./logger')
 
+const unneededVariable = 'This variable is never used'
+
 process.on('uncaughtException', (err, origin) => {
   logger.fatal({ err, origin }, 'uncaughtException')
   throw err
