db2eacae083adab979602c56046834bc6dd231d1
Nikita Mezhenskyi <nikita.mezhenskyi@gmail.com>
2022-09-19 21:13:57 -0400

    fix

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ require('dotenv').config()
 
 const logger = require('./logger')
 
-const unneededVariable = 'This variable is never used'
-
 process.on('uncaughtException', (err, origin) => {
   logger.fatal({ err, origin }, 'uncaughtException')
   throw err
