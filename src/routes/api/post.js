876aaa6d98d196086240171debac7b05ce28e1ce
Nikita Mezhenskyi <nikita.mezhenskyi@gmail.com>
2022-10-04 20:46:04 -0400

    refactor: error handling

--- src/routes/api/post.js
+++ src/routes/api/post.js
@@ -1,23 +1,35 @@
-const { createSuccessResponse, createErrorResponse } = require('../../response')
+const { createSuccessResponse } = require('../../response')
 const { Fragment } = require('../../model/fragment')
+const { ApiError } = require('../../exceptions')
 const logger = require('../../logger')
 
 if (!process.env.API_URL) {
   throw new Error('missing env vars: API_URL not found')
 }
 
-const postFragments = async (req, res) => {
+/**
+ * Creates a new fragment for current the user.
+ */
+const postFragment = async (req, res, next) => {
   if (!Buffer.isBuffer(req.body)) {
     logger.warn('POST /fragments received unsupported media type')
-    return res.status(415).json(createErrorResponse(415, 'Unsupported Media Type'))
+    return next(ApiError.UnsupportedMediaType())
   }
 
-  const fragment = new Fragment({ ownerId: req.user, type: req.get('Content-Type') })
-  await fragment.save()
-  await fragment.setData(req.body)
+  try {
+    const fragment = new Fragment({
+      ownerId: req.user,
+      type: req.get('Content-Type'),
+    })
 
-  res.set('Location', `${process.env.API_URL}/v1/fragments/${fragment.id}`)
-  return res.status(201).json(createSuccessResponse({ fragment }))
+    await fragment.save()
+    await fragment.setData(req.body)
+
+    res.set('Location', `${process.env.API_URL}/v1/fragments/${fragment.id}`)
+    return res.status(201).json(createSuccessResponse({ fragment }))
+  } catch (err) {
+    return next(err)
+  }
 }
 
-module.exports = postFragments
+module.exports.postFragment = postFragment
