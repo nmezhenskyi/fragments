8a8ff0e5c9d1e83cbfa01b080f1ae85d44c48ca4
Nikita Mezhenskyi <nikita.mezhenskyi@gmail.com>
2022-10-06 23:08:26 -0400

    test: GET /v1/fragments/:id

--- tests/unit/get.test.js
+++ tests/unit/get.test.js
@@ -2,6 +2,20 @@ const request = require('supertest')
 const app = require('../../src/app')
 const { tearDown } = require('../../src/model/data/index')
 
+/**
+ * Parses response body to Buffer object.
+ */
+const binaryParser = (res, callback) => {
+  res.setEncoding('binary')
+  res.data = ''
+  res.on('data', (chunk) => {
+    res.data += chunk
+  })
+  res.on('end', () => {
+    callback(null, Buffer.from(res.data))
+  })
+}
+
 describe('GET /v1/fragments', () => {
   beforeEach(async () => tearDown())
 
@@ -102,6 +116,46 @@ describe('GET /v1/fragments', () => {
 })
 
 describe('GET /v1/fragments/:id', () => {
+  beforeEach(async () => tearDown())
+
   test('unauthenticated requests are denied', () =>
     request(app).get('/v1/fragments/abcd').expect(401))
+
+  test('incorrect credentials are denied', () =>
+    request(app)
+      .get('/v1/fragments/abcd')
+      .auth('invalid@email.com', 'incorrect password')
+      .expect(401))
+
+  test('receive HTTP 404 response if fragment does not exist', async () => {
+    const res = await request(app)
+      .get('/v1/fragments/abcd')
+      .auth('user1@email.com', 'password1')
+    expect(res.statusCode).toBe(404)
+    expect(res.body.status).toBe('error')
+    expect(res.body.error).toMatchObject({
+      message: 'Fragment abcd not found',
+      code: 404,
+    })
+  })
+
+  /**
+   * Depends on POST /v1/fragments.
+   */
+  test('text/plain support', async () => {
+    const payload = '8N5xWFj80a'
+    const postRes = await request(app)
+      .post('/v1/fragments')
+      .auth('user1@email.com', 'password1')
+      .set('Content-Type', 'text/plain')
+      .send(payload)
+    const res = await request(app)
+      .get(`/v1/fragments/${postRes.body.fragment?.id}`)
+      .auth('user1@email.com', 'password1')
+      .buffer()
+      .parse(binaryParser)
+    expect(res.statusCode).toBe(200)
+    expect(res.headers['content-type']).toMatch(/^text\/plain/)
+    expect(res.body.toString()).toEqual(payload)
+  })
 })
