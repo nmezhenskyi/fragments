6bb59558ab771149cf968c294e95b718da7cb821
Nikita Mezhenskyi <nikita.mezhenskyi@gmail.com>
2022-10-22 19:18:08 -0400

    test: getFragmentMetadataById

--- tests/unit/get.test.js
+++ tests/unit/get.test.js
@@ -113,6 +113,18 @@ describe('GET /v1/fragments', () => {
     expect(res.body.fragments.length).toEqual(payloads.length)
     expect(res.body.fragments).toEqual(fragments)
   })
+
+  test('validate expand query parameter', async () => {
+    const res = await request(app)
+      .get(`/v1/fragments?expand=abcd`)
+      .auth('user1@email.com', 'password1')
+    expect(res.statusCode).toBe(400)
+    expect(res.body.status).toEqual('error')
+    expect(res.body.error).toEqual({
+      code: 400,
+      message: `Invalid value for 'expand' query parameter. Expected '1', got 'abcd' instead.`,
+    })
+  })
 })
 
 describe('GET /v1/fragments/:id', () => {
@@ -157,5 +169,47 @@ describe('GET /v1/fragments/:id', () => {
     expect(res.statusCode).toBe(200)
     expect(res.headers['content-type']).toMatch(/^text\/plain/)
     expect(res.body.toString()).toEqual(payload)
+  })
+})
+
+describe('GET /v1/fragments/:id/info', () => {
+  beforeEach(async () => tearDown())
+
+  test('unauthenticated requests are denied', () =>
+    request(app).get('/v1/fragments/abcd/info').expect(401))
+
+  test('incorrect credentials are denied', () =>
+    request(app)
+      .get('/v1/fragments/abcd/info')
+      .auth('invalid@email.com', 'incorrect password')
+      .expect(401))
+
+  test("receive HTTP 404 not found if fragment doesn't exist", async () => {
+    const res = await request(app)
+      .get('/v1/fragments/abcd/info')
+      .auth('user1@email.com', 'password1')
+    expect(res.statusCode).toBe(404)
+    expect(res.body.status).toEqual('error')
+    expect(res.body.error).toEqual({
+      code: 404,
+      message: 'Fragment abcd not found',
+    })
+  })
+
+  /**
+   * Depends on POST /v1/fragments.
+   */
+  test('receive fragment metadata if fragment exists', async () => {
+    const postRes = await request(app)
+      .post('/v1/fragments')
+      .auth('user1@email.com', 'password1')
+      .set('Content-Type', 'text/plain')
+      .send('8N5xWFj80a')
+    const res = await request(app)
+      .get(`/v1/fragments/${postRes.body?.fragment?.id}/info`)
+      .auth('user1@email.com', 'password1')
+    expect(res.statusCode).toBe(200)
+    expect(res.body.status).toEqual('ok')
+    expect(res.body.fragment).toEqual(postRes.body.fragment)
   })
 })
